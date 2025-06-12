2c2b0d0fa6677341f33e48f58e2ecd22089f2290
Laurent LOUIS-THERESE <laurent@bcsirt.fr>
2025-06-11 21:04:33 -0400

    FEAT : Création de la classe CategorieTournoi.php

--- app/Models/CategorieTournoi.php
+++ app/Models/CategorieTournoi.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CategorieTournoi extends Model
 {
-    use HasFactory;
+    protected $fillable = ['nom'];
+
+    // Relationships
+    public function tournois() { return $this->hasMany(Tournoi::class); }
 }
